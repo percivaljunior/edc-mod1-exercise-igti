f8e631d178bd0f601cb2ded1ff21a4c1b740c3b1
junior <pejrbb@bb.com.br>
2021-08-18 19:49:45 -0300

    job glue spark

--- infrastructure/glue_job_spark.tf
+++ infrastructure/glue_job_spark.tf
@@ -1,8 +1,12 @@
 resource "aws_glue_job" "GLUE_JOB_SPARK" {
-  name     = "GLUE_JOB_SPARK"
-  role_arn = "arn:aws:iam::449441604967:role/glue-s3-percival"
+    name     = "GLUE_JOB_SPARK"
+    role_arn = "arn:aws:iam::449441604967:role/glue-s3-percival"
 
-  command {
-    script_location = "s3://terraform-state-igti-percival/emr-code/pyspark/job_spark_from_tf.py"
-  }
+    command {
+        script_location = "s3://terraform-state-igti-percival/emr-code/pyspark/job_spark_from_tf.py"
+    }
+
+    default_arguments = {
+        "--job-language" = "spark"
+    }
 }
